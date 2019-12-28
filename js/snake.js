5ff7c0bc3f867edecea472cd90afb054129a433c
Artem Orlov <artm.orlov.88@list.ru>
2019-12-28 22:22:26 +0300

    added class blink

--- js/snake.js
+++ js/snake.js
@@ -10,10 +10,13 @@
             this._id;
             this._delay = 250;
 
+            this._blink = new Blink(this._field, this.getNum(this.getWidthCss(this._field)), this.getNum(this.getHeightCss(this._field)), this._step);
+
             this._elems = this._snake.getSquares();
             this._coordinate = this._snake.getCoordinate(this._elems);
 
             this.run();
+            console.log(this._blink.getTopLeft());
         }
 
         run() {
@@ -23,6 +26,8 @@
                 this._id = setInterval(() => {
                     let left = parseInt(this._elems[this._elems.length - 1].style.left) + this._step + 'px';
                     let top = parseInt(this._elems[this._elems.length - 1].style.top) + 'px';
+
+                    this.catch(this._blink.getTopLeft(), left);
 
                     this.checkOnCollisionWithBorderLeftAndDown(this.getWidthCss(this._field), left, this._id);
 
@@ -40,6 +45,8 @@
                             let left = parseInt(this._elems[this._elems.length - 1].style.left) - this._step + 'px';
                             let top = parseInt(this._elems[this._elems.length - 1].style.top) + 'px';
 
+                            this.catch(this._blink.getTopLeft(), left);
+
                             this.checkOnCollisionWithBorderRightAndUp('0px', left, this._id);
     
                             new Move(this._elems, this._coordinate, top, left);
@@ -52,6 +59,8 @@
                         this._id = setInterval(() => {
                             let left = parseInt(this._elems[this._elems.length - 1].style.left) + 'px';
                             let top = parseInt(this._elems[this._elems.length - 1].style.top) - this._step + 'px';
+
+                            this.catch(this._blink.getTopLeft(), top);
                             
                             this.checkOnCollisionWithBorderRightAndUp('0px', top, this._id);
     
@@ -66,6 +75,8 @@
                             let left = parseInt(this._elems[this._elems.length - 1].style.left) + this._step + 'px';
                             let top = parseInt(this._elems[this._elems.length - 1].style.top) + 'px';
 
+                            this.catch(this._blink.getTopLeft(), left);
+
                             this.checkOnCollisionWithBorderLeftAndDown(this.getWidthCss(this._field), left, this._id);
     
                             new Move(this._elems, this._coordinate, top, left);
@@ -78,6 +89,8 @@
                         this._id = setInterval(() => {
                             let left = parseInt(this._elems[this._elems.length - 1].style.left) + 'px';
                             let top = parseInt(this._elems[this._elems.length - 1].style.top) + this._step + 'px';
+
+                            this.catch(this._blink.getTopLeft(), top);
 
                             this.checkOnCollisionWithBorderLeftAndDown(this.getHeightCss(this._field), top, this._id);
 
@@ -115,6 +128,18 @@
                 clearInterval(id);
                 this._lose.showMessage();
             }
+        }
+
+        catch(arr, coordinate) {
+            for (let elem of arr) {
+                if (elem == coordinate) {
+                    console.log('yes');
+                }
+            }
+        } 
+
+        getNum(str) {
+            return parseInt(str);
         }
     }
 
@@ -230,6 +255,23 @@
 
         getButton() {
             return  this._button;
+        }
+    }
+    class Blink {
+        constructor(parent, width, height, step) {
+            this._div = document.createElement('div');
+            this._div.classList.add('blink');
+                this._div.style.top = this.getRandomInt(10, height - step) + 'px';
+                this._div.style.left = this.getRandomInt(10, width - step) + 'px';
+            parent.appendChild(this._div);
+        }
+
+        getRandomInt(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+
+        getTopLeft() {
+            return [this._div.style.top, this._div.style.left];
         }
     }
 
